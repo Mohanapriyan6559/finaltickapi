39b75687d00511dab391d2fea3f7324c4cd89e73
Mohanapriyan6559 <mohanapriyan.ds.mfedu@gmail.com>
2025-04-16 20:02:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 from heapq import nlargest
-import fitz
+
 # ----------------- PAGE CONFIG -----------------
 st.set_page_config(page_title="Smart Learning AI", layout="wide")
 
